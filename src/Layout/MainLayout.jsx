74241099de3d12c0659d2ffe874477798a921ba0
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-05-26 19:11:04 +0400

    Navbar added.

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/Footer/Footer";
+import Navbar from "../pages/shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
       <Footer />
     </>
---
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -0,0 +1,80 @@
+import { Link } from "react-router-dom";
+import cart from "../../../assets/cart.png";
+import textLogo from "../../../assets/text-logo.png";
+
+const Navbar = () => {
+  const navListItems = (
+    <div className="font-medium md:flex">
+      <li>
+        <Link to="">HOME</Link>
+      </li>
+      <li>
+        <Link to="">CONTACT US</Link>
+      </li>
+      <li>
+        <Link to="">DASHBOARD</Link>
+      </li>
+      <li>
+        <Link to="">OUR MENU</Link>
+      </li>
+      <li>
+        <Link to="">OUR SHOP</Link>
+      </li>
+      <li>
+        <Link to="">
+          <img src={cart} alt="" className="h-11" />
+        </Link>
+      </li>
+      <li>
+        <Link to="">SIGN OUT</Link>
+      </li>
+      <li>
+        <Link to="">
+          <img
+            src="https://img.freepik.com/free-icon/user_318-824560.jpg?w=360"
+            alt=""
+            className="w-9 h-9 rounded-full"
+          />
+        </Link>
+      </li>
+    </div>
+  );
+  return (
+    <div className="flex justify-between bg-black text-white px-10 md:px-14">
+      <div className="flex">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navListItems}
+          </ul>
+        </div>
+        <Link to="">
+          <img src={textLogo} alt="" className="h-[75px] my-3" />
+        </Link>
+      </div>
+      <div className="hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navListItems}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
