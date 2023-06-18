18447e526c51615b2b1b7b9a2c5637a020884f77
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-06-18 10:34:56 +0400

    Delete item from server added.

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,7 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 
 const useMenu = () => {
-  const {refetch, data: menu = [], isLoading: loading } = useQuery({
+  const {
+    data: menu = [],
+    isLoading: loading,
+    refetch,
+  } = useQuery({
     queryKey: ["menu"],
     queryFn: async () => {
       const res = await fetch("http://localhost:3000/menu");
--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -2,10 +2,34 @@ import { Helmet } from "react-helmet-async";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
-  const [menu] = useMenu();
+  const [menu, , refetch] = useMenu();
+  const [axiosSecure] = useAxiosSecure();
 
+  const handelItemDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/menu/${item._id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire("Deleted!", "Your item has been deleted.", "success");
+          }
+        });
+      }
+    });
+  };
   return (
     <>
       <Helmet>
@@ -59,7 +83,7 @@ const ManageItems = () => {
                   </td>
                   <td>
                     <button
-                      // onClick={() => handelItemDelete(item)}
+                      onClick={() => handelItemDelete(item)}
                       className="btn bg-[#B91C1C] border-0"
                     >
                       <FaTrashAlt />
